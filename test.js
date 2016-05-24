0d106c442ccfa6b5bffca0023b9596189612298d
Ben Drucker <bvdrucker@gmail.com>
2016-05-24 12:27:58 -0700

    Add tests asserting that ES6 syntax is not present in output bundle

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
 const test = require('tape')
+const browserify = require('browserify')
 const es2020 = require('./')
 
-test('should assert input types', function (t) {
-  t.plan(1)
-  t.throws(es2020)
+test('es2020', function (t) {
+  t.plan(3)
+
+  browserify({
+    debug: true
+  })
+  .add('example.js')
+  .transform(es2020)
+  .bundle(function (err, code) {
+    if (err) return t.end(err)
+    code = String(code)
+
+    t.notOk(code.includes('=>'), 'transpiles arrow functions')
+    t.notOk(code.includes('`'), 'transpiles template literals')
+    t.notOk(code.includes('`'), 'transpiles const assignments')
+  })
 })
